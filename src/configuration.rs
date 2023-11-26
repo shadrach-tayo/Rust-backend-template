ecb42d7914eb2efedab3cb90f80f32dc185701ab
shadrach-tayo <shadrachtemitayo@gmail.com>
2023-11-26 14:56:22 +0100

    add sqlx postgres support

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,5 +1,8 @@
+use secrecy::{ExposeSecret, Secret};
 use serde_derive::Deserialize;
 use serde_aux::field_attributes::deserialize_number_from_string;
+use sqlx::ConnectOptions;
+use sqlx::postgres::{PgConnectOptions, PgSslMode};
 
 #[derive(Deserialize, Clone)]
 pub struct Configuration {
@@ -10,12 +13,56 @@ pub struct Configuration {
 #[derive(Deserialize, Clone)]
 pub struct DatabaseSettings {
     pub username: String,
-    pub password: String,
+    pub password: Secret<String>,
     #[serde(deserialize_with = "deserialize_number_from_string")]
     pub port: u16,
     pub host: String,
     pub database_name: String,
     pub require_ssl: bool,
+}
+
+impl DatabaseSettings {
+    pub fn without_db(&self) -> PgConnectOptions {
+        let ssl_mode = if self.require_ssl {
+            PgSslMode::Require
+        } else {
+            PgSslMode::Prefer
+        };
+
+        PgConnectOptions::new()
+            .host(&self.host)
+            .port(self.port)
+            .username(&self.username)
+            .password(&self.password.expose_secret())
+            .ssl_mode(ssl_mode)
+    }
+
+    pub fn with_db(&self) -> PgConnectOptions {
+        let mut options = self.without_db().database(&self.database_name);
+        options.log_statements(tracing_log::log::LevelFilter::Trace);
+        options
+    }
+
+    pub fn connection_string(&self) ->  Secret<String> {
+        Secret::new(format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+            self.database_name
+        ))
+    }
+
+    pub fn connection_string_without_db(&self) -> Secret<String> {
+        Secret::new(format!(
+            "postgres://{}:{}@{}:{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port
+        ))
+    }
 }
 
 #[derive(Deserialize, Clone)]
--- src/startup.rs
+++ src/startup.rs
@@ -1,7 +1,10 @@
 use std::net::TcpListener;
 use actix_web::dev::Server;
 use actix_web::{App, HttpServer, web};
-use crate::configuration::Configuration;
+use actix_web::web::Data;
+use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
+use crate::configuration::{Configuration, DatabaseSettings};
 use crate::routes::{health_check, home};
 
 pub struct Application {
@@ -14,7 +17,10 @@ impl Application {
         let address = format!("{}:{}", settings.application.host, settings.application.port);
         let listener = TcpListener::bind(address).expect("Failed to bind port");
         let port = listener.local_addr().unwrap().port();
-        let server = run(listener, settings.application.base_url)
+
+        let connection_pool = get_connection_pool(&settings.database);
+
+        let server = run(listener, connection_pool, settings.application.base_url)
             .await?;
         Ok(Self { port, server })
     }
@@ -26,19 +32,27 @@ impl Application {
 
 pub struct ApplicationBaseUrl(pub String);
 
-pub async fn run(listener: TcpListener, base_url: String) -> Result<Server, anyhow::Error> {
+pub async fn run(listener: TcpListener, db_pool: PgPool, base_url: String) -> Result<Server, anyhow::Error> {
     let port = listener.local_addr().unwrap().port();
     tracing::info!("starting server at http://localhost:{}", port);
 
-    let base_url = web::Data::new(ApplicationBaseUrl(base_url));
+    let db_connection = Data::new(db_pool);
+
+    let base_url = Data::new(ApplicationBaseUrl(base_url));
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/", web::get().to(home))
             .app_data(base_url.clone())
+            .app_data(db_connection.clone())
     })
 
         .listen(listener)?
         .run();
     Ok(server)
+}
+
+pub fn get_connection_pool(database_configuration: &DatabaseSettings) -> PgPool {
+    PgPoolOptions::new().acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy_with(database_configuration.with_db())
 }
